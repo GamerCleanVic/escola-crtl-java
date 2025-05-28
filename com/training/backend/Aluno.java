c953005fc39645f96cd78ba3acce4990e4c54a02
GamerCleanVic <jonassd2000@gmail.com>
2025-05-28 18:42:17 -0500

    Pessoa

---
+++ com/training/backend/Aluno.java
@@ -0,0 +1,5 @@
+package com.training.backend;
+
+public class Aluno extends Pessoa{
+	
+}
---
+++ com/training/backend/Pessoa.java
@@ -0,0 +1,11 @@
+package com.training.backend;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	private int idade;
+	private String sexo;
+	private String login;
+	private String senha;
+	private int id;
+}
