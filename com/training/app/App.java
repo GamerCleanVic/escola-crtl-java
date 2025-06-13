27d04b8aac60488597d22fcd3fd4472ff8f0381a
GamerCleanVic <jonassd2000@gmail.com>
2025-06-13 16:35:32 -0500

    Aprovado || Reprovado

--- com/training/app/App.java
+++ com/training/app/App.java
@@ -17,20 +17,38 @@ public class App {
 		al1.setN1(n2);
 		al1.setN1(n3);
 		al1.setN1(n4);
-		al1.somar(n1, n2, n3, n4);
+		al1.somar(n1, n2, n3, n4);		
 		
-		JOptionPane.showMessageDialog(null, "Nome: "+al1.getNome()+"\nMédia: "+al1.mediaDoAluno()
-		, "Resultado", (al1.mediaDoAluno() < 7) ? JOptionPane.WARNING_MESSAGE : 
-			JOptionPane.INFORMATION_MESSAGE);
-		
-		if(al1.mediaDoAluno() < 0 || al1.somar(n1, n2, n3, n4) <= 0) {
-			JOptionPane.showMessageDialog(null, "\nTirou média "+al1.mediaDoAluno()+" ou menor"
-			, "Reprovado", JOptionPane.ERROR_MESSAGE);
-		}else {
-			JOptionPane.showMessageDialog(null, "\nPor quanto fazer a final: "
-			+al1.porQuantoFazerFinal()
-			, "Na Final", JOptionPane.WARNING_MESSAGE);
+		boolean auxVetor = true;
+		if(Double.isNaN(al1.getN1()) || Double.isNaN(al1.getN2()) || Double.isNaN(al1.getN3()) 
+				|| Double.isNaN(al1.getN4())) {
+			auxVetor = false;
 		}
 		
+			if(al1.getNome() == null || al1.getNome().isEmpty() || auxVetor == false) {
+				JOptionPane.showMessageDialog(null, "Não pode deixar o campo vazio!"
+						, "Preencha a caixa de texto", JOptionPane.WARNING_MESSAGE);
+			}else {			
+				JOptionPane.showMessageDialog(null, "Nome: "+al1.getNome()+"\nMédia: "+al1.mediaDoAluno()
+				, "Resultado", (al1.mediaDoAluno() < 7) ? JOptionPane.WARNING_MESSAGE : 
+					JOptionPane.INFORMATION_MESSAGE);
+				
+				if(al1.mediaDoAluno() < 0 || al1.somar(n1, n2, n3, n4) <= 0) {
+					JOptionPane.showMessageDialog(null, "\nMédia zero ou menor: "+al1.mediaDoAluno()
+					, "Reprovado", JOptionPane.ERROR_MESSAGE);
+				}else if(al1.mediaDoAluno() >= 7){
+					JOptionPane.showMessageDialog(null, "\nAluno aprovado! Média: "
+					+al1.mediaDoAluno()
+					, "Aprovado", JOptionPane.INFORMATION_MESSAGE);
+				}else if(al1.mediaDoAluno() < 5){
+					JOptionPane.showMessageDialog(null, "\nAluno reprovado! Média < 5: "
+					+al1.mediaDoAluno()
+					, "Reprovado", JOptionPane.ERROR_MESSAGE);
+				}else {
+					JOptionPane.showMessageDialog(null, "\nPor quanto fazer a final: "
+							+al1.porQuantoFazerFinal()
+							, "Na Final", JOptionPane.WARNING_MESSAGE);
+				}
+			}
 	}
 }
